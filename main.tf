b31e4b8c276940ef353effffb3c95ed2ce674466
berko-v <idoberko6@gmail.com>
2025-12-25 17:05:21 +0200

    split lambda iam roles and tighten the permissions

--- main.tf
+++ main.tf
@@ -18,9 +18,9 @@ provider "aws" {
   profile = "default"
 }
 
-# IAM stays in root (shared by both Lambdas)
-resource "aws_iam_role" "lambda_role" {
-  name = "${var.project}-${var.env}-lambda-role"
+# IAM roles (split per function)
+resource "aws_iam_role" "lambda_root_role" {
+  name = "${var.project}-${var.env}-lambda-root-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -32,17 +32,26 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-resource "aws_iam_role_policy" "lambda_policy" {
-  role = aws_iam_role.lambda_role.id
+resource "aws_iam_role" "lambda_items_role" {
+  name = "${var.project}-${var.env}-lambda-items-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+# CloudWatch Logs permissions (needed by both Lambdas)
+resource "aws_iam_role_policy" "lambda_root_logs" {
+  role = aws_iam_role.lambda_root_role.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      {
-        Effect   = "Allow"
-        Action   = ["dynamodb:PutItem", "dynamodb:Scan"]
-        Resource = module.db.arn
-      },
       {
         Effect = "Allow"
         Action = [
@@ -51,6 +60,41 @@ resource "aws_iam_role_policy" "lambda_policy" {
           "logs:PutLogEvents"
         ]
         Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_items_logs" {
+  role = aws_iam_role.lambda_items_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# DynamoDB permissions (ONLY for /items handler)
+resource "aws_iam_role_policy" "lambda_items_dynamodb" {
+  role = aws_iam_role.lambda_items_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["dynamodb:PutItem", "dynamodb:Scan"]
+        Resource = module.db.arn
       }
     ]
   })
@@ -67,7 +111,7 @@ module "lambda_root" {
   source        = "./modules/lambda"
   function_name = "${var.project}-${var.env}-lambda"
   handler       = "handler.root_handler"
-  role_arn      = aws_iam_role.lambda_role.arn
+  role_arn      = aws_iam_role.lambda_root_role.arn
   source_dir    = "${path.root}/lambda"
   env_vars = {
     TABLE_NAME = module.db.name
@@ -80,7 +124,7 @@ module "lambda_items" {
   source        = "./modules/lambda"
   function_name = "${var.project}-${var.env}-items"
   handler       = "handler.items_handler"
-  role_arn      = aws_iam_role.lambda_role.arn
+  role_arn      = aws_iam_role.lambda_items_role.arn
   source_dir    = "${path.root}/lambda"
   env_vars = {
     TABLE_NAME = module.db.name
